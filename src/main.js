e6988acbb4232886ab8a15c3d7055d0ee3651bdc
another <schibkoartem@yandex.ru>
2025-08-02 15:56:25 +0400

    the final commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-import './style.css'
-import Button from './components/UI/Button'
+import './style.scss'
+import Button from './components/UI/Button/Button'
+
+document.querySelector("#app").innerHTML = `
+  <section>
+    ${Button()}
+    ${Button()}
+    ${Button()}
+    <img src="/vite.svg ">
+  </section>
+`
 
